ebcb7659956365d3f2daf9c72783ca8759adbfed
Chethina2003 <kovida.fernando@gmail.com>
2025-12-24 10:47:03 +0530

    Added propertycard

--- property-master/src/pages/Properties.jsx
+++ property-master/src/pages/Properties.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "../data/properties.json";
 import "./Properties.css";
+import PropertyCard from "../components/PropertyCard";
 import Navigation from "../components/Navigation.jsx";
 
 function Properties() {
@@ -9,6 +10,16 @@ function Properties() {
   return (
     <>
     <Navigation />
+
+    <div className="properties-page">
+      <h1 className="properties-title">Available Properties</h1>
+
+      <div className="properties-grid">
+        {properties.map((property) => (
+          <PropertyCard key={property.id} property={property} />
+        ))}
+      </div>
+    </div>
     
     </>
   );
