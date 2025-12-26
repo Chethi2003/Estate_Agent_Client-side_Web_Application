18ab7a60122c58203793d568bb243b47e41fbcf0
Chethina2003 <kovida.fernando@gmail.com>
2025-12-26 10:23:24 +0530

    nujll

--- property-master/src/pages/Home.jsx
+++ property-master/src/pages/Home.jsx
@@ -1,57 +1,25 @@
-import { useState } from "react";
-import "./Home.css";
+import { useState } from 'react'
+import './Home.css'
+import Navigation from '../components/Navigation.jsx'
+import Welcome from '../components/Welcome.jsx'
+import Search from '../components/Search.jsx'
+import Favourites from '../components/Favourites.jsx'
+import Footer from '../components/Footer.jsx'
 
-import Navigation from "../components/Navigation.jsx";
-import Welcome from "../components/Welcome.jsx";
-import Search from "../components/Search.jsx";
-import Favourites from "../components/Favourites.jsx";
-import Footer from "../components/Footer.jsx";
-import PropertyCard from "../components/PropertyCard.jsx";
-
-import properties from "../data/properties";
-
-function Home() {
-  const [filteredProperties, setFilteredProperties] = useState(properties);
-  const [favourites, setFavourites] = useState([]);
-
-  // ❤️ Toggle favourite
-  const toggleFavourite = (propertyId) => {
-    setFavourites((prev) =>
-      prev.includes(propertyId)
-        ? prev.filter((id) => id !== propertyId)
-        : [...prev, propertyId]
-    );
-  };
+function App() {
 
   return (
     <>
       <Navigation />
-      <Welcome />
-
+        <Welcome />
       <div className="main-content">
-        <Search onResults={setFilteredProperties} />
-
-        {/* 🔍 SEARCH RESULTS */}
-        <div className="results-grid">
-          {filteredProperties.map((property) => (
-            <PropertyCard
-              key={property.id}
-              property={property}
-              isFavourite={favourites.includes(property.id)}
-              onToggleFavourite={toggleFavourite}
-            />
-          ))}
-        </div>
-
-        <Favourites
-          favourites={favourites}
-          properties={properties}
-        />
+        <Search />
+        <Favourites />
       </div>
-
       <Footer />
+      
     </>
-  );
+  )
 }
 
-export default Home;
+export default App
