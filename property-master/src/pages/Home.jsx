c8e1dfdcad2e3005c2b7d197e99fb9c3dbc3599e
Chethina2003 <kovida.fernando@gmail.com>
2025-12-26 10:34:37 +0530

    Update the Home.jsx for functionality

--- property-master/src/pages/Home.jsx
+++ property-master/src/pages/Home.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
-import './Home.css'
-import Navigation from '../components/Navigation.jsx'
-import Welcome from '../components/Welcome.jsx'
-import Search from '../components/Search.jsx'
-import Favourites from '../components/Favourites.jsx'
-import Footer from '../components/Footer.jsx'
+import { useState } from "react";
+import "./Home.css";
 
-function App() {
+import Navigation from "../components/Navigation.jsx";
+import Welcome from "../components/Welcome.jsx";
+import Search from "../components/Search.jsx";
+import Favourites from "../components/Favourites.jsx";
+import Footer from "../components/Footer.jsx";
+
+function Home() {
+  // 🧠 shared state (not used yet)
+  const [favourites, setFavourites] = useState([]);
 
   return (
     <>
       <Navigation />
-        <Welcome />
+      <Welcome />
+
       <div className="main-content">
         <Search />
         <Favourites />
       </div>
+
       <Footer />
-      
     </>
-  )
+  );
 }
 
-export default App
+export default Home;
