60eb68610098a49075f5930b430fb97084633eb4
Chethina2003 <kovida.fernando@gmail.com>
2025-12-28 11:28:08 +0530

    null

--- property-master/src/App.jsx
+++ property-master/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import Navigation from "./components/Navigation";
 
 function App() {
   return (
